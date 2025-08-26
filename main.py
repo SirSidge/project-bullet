057e7c05c13d1e798b44ac92cb1a3effb49b18f7
Heinz <heinz.hscharlach@gmail.com>
2025-08-26 17:19:01 +0200

    Added main menu asset and image resolution scallability

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((320, 180))
+
+# Base resolutiuon
+BASE_WIDTH, BASE_HEIGHT = 320, 180
+# Display resolution
+DISPLAY_WIDTH, DISPLAY_HEIGHT = 1280, 720
+SCALE_FACTOR = DISPLAY_WIDTH // BASE_WIDTH
+
+# Create surfaces
+logic_surface = pygame.Surface((BASE_WIDTH, BASE_HEIGHT))
+display_surface = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+# Load main menu
+main_menu = pygame.image.load("assets/main-menu/main-menu.png")
+
+#screen = pygame.display.set_mode((1280, 720))
+
 pygame.display.set_caption("Project Bullet")
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0, 0, 0))
+    # Clear logic surface (paint black)
+    logic_surface.fill((0, 0, 0))
+
+    # Draw main menu in native (Base resolution)
+    logic_surface.blit(main_menu, (0, 0))
+
+    # Scale logic surface to display surface
+    pygame.transform.scale(logic_surface, (DISPLAY_WIDTH, DISPLAY_HEIGHT), display_surface)
+
+    # Update display
     pygame.display.flip()
 pygame.quit()
